960958879d77fa5b5fa1ecb23d7606576aa4d705
Francisco <franciscoalbarracincordoba@gmail.com>
2023-06-30 11:06:03 -0300

    jajaas

--- java/jarvis.js
+++ java/jarvis.js
@@ -4,13 +4,36 @@ let selectedDestinations = []; // Almacena los índices de los destinos seleccio
 let selectedTransportation = []; // Almacena los índices de las opciones de transporte seleccionadas
 let total = 0; // Almacena el total
 
+// Función para seleccionar un paquete
+function selectPackage(packageIndex) {
+    selectedPackages = [packageIndex]; // Guardar el índice del paquete seleccionado en el arreglo de selecciones de paquete
+    total = 10; // Establecer el total inicial en $10 para el paquete
+    updateSummary(); // Actualizar el resumen
+}//La función "selectPackage" se utiliza para seleccionar un paquete específico dentro de un sistema. Toma como argumento el índice del paquete que se desea seleccionar y lo guarda en un arreglo llamado "selectedPackages". Además, establece un valor inicial de $10 para el total del paquete seleccionado y luego llama a la función "updateSummary" para actualizar el resumen.
+
+// Función para seleccionar un destino
+function selectDestination(destinationIndex) {
+    selectedDestinations = [destinationIndex]; // Guardar el índice del destino seleccionado en el arreglo de selecciones de destino
+    total = 10; // Establecer el total inicial en $10 para el destino
+    updateSummary(); // Actualizar el resumen
+}//Esta función se encarga de seleccionar un destino a partir de un índice dado. El índice del destino seleccionado se guarda en un arreglo llamado "selectedDestinations". Además, se establece un total inicial de $10 para ese destino. Luego, se llama a la función "updateSummary()" para actualizar el resumen.
+
+// Función para seleccionar una opción de transporte
+function selectTransportation(transportationIndex) {
+    selectedTransportation = [transportationIndex]; // Guardar el índice de la opción de transporte seleccionada en el arreglo de selecciones de transporte
+    total = 10; // Establecer el total inicial en $10 para la opción de transporte
+    updateSummary(); // Actualizar el resumen
+}//La función "selectTransportation" se utiliza para seleccionar una opción de transporte. Toma como argumento el índice de la opción de transporte seleccionada y guarda este índice en una variable llamada "selectedTransportation". Además, establece el total inicial en $10 para la opción de transporte seleccionada y luego llama a la función "updateSummary" para actualizar el resumen.
+
+
 // Función para agregar una selección al resumen
 function addToSummary(item, category) {
     const summaryItems = document.getElementById('summary-items'); // Obtiene el elemento del resumen
     const listItem = document.createElement('li'); // Crea un nuevo elemento de lista
     listItem.textContent = `${category}: ${item}`; // Establece el texto del elemento de lista con la categoría y el elemento seleccionado
     summaryItems.appendChild(listItem); // Agrega el elemento de lista al resumen appendChild es un método en JavaScript que se utiliza para agregar un nodo como hijo de otro nodo existente en el árbol DOM (Document Object Model). Se puede utilizar para agregar elementos HTML, como elementos de lista, párrafos, imágenes, etc., como hijos de otros elementos existentes en el documento HTML.
-}//La función "updateTotal" se utiliza para actualizar el valor total y mostrarlo en el resumen. Primero, se obtiene el elemento del total en el resumen mediante su ID. Luego, se crea una variable local llamada "total" para almacenar el nuevo valor total. 
+}
+//La función "updateTotal" se utiliza para actualizar el valor total y mostrarlo en el resumen. Primero, se obtiene el elemento del total en el resumen mediante su ID. Luego, se crea una variable local llamada "total" para almacenar el nuevo valor total. 
 // A continuación, se calcula el total según las selecciones de paquete. Se recorre el array "selectedPackages" y se suma una cantidad específica al total dependiendo del índice del paquete seleccionado. 
 // Después, se calcula el total según las selecciones de destino. Se recorre el array "selectedDestinations" y se suma una cantidad específica al total dependiendo del índice del destino seleccionado. 
 // Finalmente, se calcula el total según las selecciones de transporte. Se recorre el array "selectedTransportation" y se suma una cantidad específica al total dependiendo del índice del transporte seleccionado. 
@@ -63,28 +86,7 @@ function updateTotal() {
 // Una vez calculado el nuevo total, se actualiza el elemento HTML con el valor correspondiente utilizando la propiedad "textContent" del elemento. 
 // Por favor, avísame si tienes alguna pregunta adicional.
 
-// Función para seleccionar un paquete
-function selectPackage(packageIndex) {
-    selectedPackages = [packageIndex]; // Guardar el índice del paquete seleccionado en el arreglo de selecciones de paquete
-    total = 10; // Establecer el total inicial en $10 para el paquete
-    updateSummary(); // Actualizar el resumen
-}//La función "selectPackage" se utiliza para seleccionar un paquete específico dentro de un sistema. Toma como argumento el índice del paquete que se desea seleccionar y lo guarda en un arreglo llamado "selectedPackages". Además, establece un valor inicial de $10 para el total del paquete seleccionado y luego llama a la función "updateSummary" para actualizar el resumen.
-
-// Función para seleccionar un destino
-function selectDestination(destinationIndex) {
-    selectedDestinations = [destinationIndex]; // Guardar el índice del destino seleccionado en el arreglo de selecciones de destino
-    total = 10; // Establecer el total inicial en $10 para el destino
-    updateSummary(); // Actualizar el resumen
-}//Esta función se encarga de seleccionar un destino a partir de un índice dado. El índice del destino seleccionado se guarda en un arreglo llamado "selectedDestinations". Además, se establece un total inicial de $10 para ese destino. Luego, se llama a la función "updateSummary()" para actualizar el resumen.
-
-// Función para seleccionar una opción de transporte
-function selectTransportation(transportationIndex) {
-    selectedTransportation = [transportationIndex]; // Guardar el índice de la opción de transporte seleccionada en el arreglo de selecciones de transporte
-    total = 10; // Establecer el total inicial en $10 para la opción de transporte
-    updateSummary(); // Actualizar el resumen
-}//La función "selectTransportation" se utiliza para seleccionar una opción de transporte. Toma como argumento el índice de la opción de transporte seleccionada y guarda este índice en una variable llamada "selectedTransportation". Además, establece el total inicial en $10 para la opción de transporte seleccionada y luego llama a la función "updateSummary" para actualizar el resumen.
-
-// Función para actualizar el resumen
+//Función para actualizar el resumen
 function updateSummary() {
     const summaryItems = document.getElementById('summary-items'); // Obtiene el elemento del resumen
     summaryItems.innerHTML = ''; // Limpiar el contenido actual del resumen
